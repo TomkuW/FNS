99d8f3fd80e02b5ec94f91bb3cb95a5dd170d93e
Tomasz Waberski <wabtomek@gmail.com>
2017-12-08 21:23:55 +0100

    Pierwszy projekt

---
+++ src/Projekt/Main.java
@@ -0,0 +1,23 @@
+package Projekt;
+
+import javafx.application.Application;
+
+import javafx.stage.Stage;
+
+
+
+/**
+ * Created by Tomek on 2017-10-13.
+ *
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+
+
+    }
+
+}
