e40b289d8ad7d3c6011a44fa837391d1673b1276
Tomasz Waberski <wabtomek@gmail.com>
2017-12-11 21:23:38 +0100

    Kontroler zakończ polączenie wyloguj

---
+++ src/Projekt/Okienka/WitajWylogujControler.java
@@ -0,0 +1,44 @@
+package Projekt.Okienka;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Tomek on 2017-11-17.
+ */
+public class WitajWylogujControler implements Initializable{
+
+    Stage dialogeStage = new Stage();
+
+
+    @FXML
+    public void wyloguj(ActionEvent event) {
+        try {
+            ((Node) event.getSource()).getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Logowanie.fxml"));
+            Pane layout = loader.load();
+            Scene scene = new Scene(layout);
+            dialogeStage.setScene(scene);
+            dialogeStage.show();
+
+        } catch (Exception e) {
+            System.err.println("Problem z zamknieciem polaczenia");
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
