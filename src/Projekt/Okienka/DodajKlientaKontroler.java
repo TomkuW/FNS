ed6f6a4ee9148f98e1185bdd2645d8fa71753219
Tomasz Waberski <wabtomek@gmail.com>
2017-12-14 23:28:02 +0100

    Zaimpementowana zakladka klienci

--- src/Projekt/Okienka/DodajKlientaKontroler.java
+++ src/Projekt/Okienka/DodajKlientaKontroler.java
@@ -1,7 +1,84 @@
 package Projekt.Okienka;
 
+import Projekt.ConntectToDB;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  * Created by Tomek on 2017-12-13.
  */
 public class DodajKlientaKontroler {
+
+    @FXML
+    private TextField imie_klienta;
+    @FXML
+    private TextField nazwisko_klienta;
+    @FXML
+    private TextField pesel_klienta;
+    @FXML
+    private TextField miejscowosc_klienta;
+    @FXML
+    private TextField ulica_klienta;
+    @FXML
+    private TextField nr_dom_klienta;
+    @FXML
+    private TextField telefon_klienta;
+    @FXML
+    private Button dodaj;
+    @FXML
+    private Button anuluj;
+
+    @FXML
+    protected void dodawanie() throws SQLException {
+
+        String imie = imie_klienta.getText();
+        String nazwisko = nazwisko_klienta.getText();
+        String PESEL = pesel_klienta.getText();
+        String miejscowosc = miejscowosc_klienta.getText();
+        String ulica = ulica_klienta.getText();
+        String nr_dom = nr_dom_klienta.getText();
+        String nr_telefon = telefon_klienta.getText();
+
+        String sql = "insert into klienci (imie,nazwisko,PESEL,miejscowosc,ulica,nr_dom,nr_telefon) values(?,?,?,?,?," +
+                "?,?)";
+        Connection conn = ConntectToDB.Connector();
+        PreparedStatement prepStmt = null;
+        try {
+
+            prepStmt = conn.prepareStatement(sql);
+            prepStmt.setString(1, imie);
+            prepStmt.setString(2, nazwisko);
+            prepStmt.setString(3, PESEL);
+            prepStmt.setString(4, miejscowosc);
+            prepStmt.setString(5, ulica);
+            prepStmt.setString(6, nr_dom);
+            prepStmt.setString(7, nr_telefon);
+            prepStmt.executeUpdate();
+
+
+            Stage stage = (Stage) dodaj.getScene().getWindow();
+            stage.close();
+
+
+        } catch (SQLException e) {
+
+            System.out.print("Błąd podczas dodawania klienta" + e);
+            throw e;
+        }
+
+    }
+
+    @FXML
+    private void przyciskAnulujDodawanie() {
+        Stage stage = (Stage) anuluj.getScene().getWindow();
+        stage.close();
+
+    }
+
 }
--- src/Projekt/Okienka/ModyfikujKlientaKontroler.java
+++ src/Projekt/Okienka/ModyfikujKlientaKontroler.java
@@ -1,7 +1,105 @@
 package Projekt.Okienka;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
 /**
  * Created by Tomek on 2017-12-13.
  */
-public class ModyfikujKlientaKontroler {
+public class ModyfikujKlientaKontroler implements Initializable {
+
+    @FXML
+    private TextField imie_k;
+    @FXML
+    private TextField nazwisko_k;
+    @FXML
+    private TextField pesel_k;
+    @FXML
+    private TextField miejscowosc_k;
+    @FXML
+    private TextField ulica_k;
+    @FXML
+    private TextField nr_dom_k;
+    @FXML
+    private TextField telefon_k;
+    @FXML
+    private Button modyfikuj;
+    @FXML
+    private Button anuluj;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        String imie;
+        String nazwisko;
+        String pesel;
+        String miejscowosc;
+        String ulica;
+        String nrdomu;
+        String telefon;
+
+
+        int klient_id = KlientKontroler.getSelectedKlientId();
+        try {
+
+            ResultSet rs = KlientDAO.wyswietlDaneKlient(klient_id);
+            rs.next();
+            imie = rs.getString("imie");
+
+            nazwisko = rs.getString("nazwisko");
+
+            pesel = rs.getString("PESEL");
+
+            miejscowosc = rs.getString("miejscowosc");
+
+            ulica = rs.getString("ulica");
+
+            nrdomu = rs.getString("nr_dom");
+
+            telefon = rs.getString("nr_telefon");
+
+
+            imie_k.setText(imie);
+            nazwisko_k.setText(nazwisko);
+            pesel_k.setText(pesel);
+            miejscowosc_k.setText(miejscowosc);
+            ulica_k.setText(ulica);
+            nr_dom_k.setText(nrdomu);
+            telefon_k.setText(telefon);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void modykikujKlienta() throws SQLException, ClassNotFoundException {
+        try {
+
+            KlientDAO.updateKlient(KlientKontroler.getSelectedKlientId(), imie_k.getText(), nazwisko_k.getText(),
+                    pesel_k.getText(), miejscowosc_k.getText(), ulica_k.getText(), nr_dom_k.getText(),
+                    telefon_k.getText());
+
+            Stage stage = (Stage) modyfikuj.getScene().getWindow();
+            stage.close();
+
+        } catch (SQLException e) {
+            System.out.print("Błąd podczas modyfikacji klienta" + e);
+        }
+    }
+    @FXML
+    private void przyciskAnulujModyfikacje() {
+        Stage stage = (Stage) anuluj.getScene().getWindow();
+        stage.close();
+
+    }
+
 }
