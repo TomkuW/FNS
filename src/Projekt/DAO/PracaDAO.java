f82ed011126dc3cd82a54f9a9589a0ebe72c5c7a
Tomasz Waberski <wabtomek@gmail.com>
2018-01-03 00:08:12 +0100

    Praca gotowa do testow

---
+++ src/Projekt/DAO/PracaDAO.java
@@ -0,0 +1,130 @@
+package Projekt.DAO;
+
+import Projekt.Modele.Zamowienie;
+import Projekt.PodlaczonieDoBazy.ConntectToDB;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.*;
+
+/**
+ * Klasa zapewniająca połączenie między kontrolerem a bazą danych
+ *
+ * Created by Tomek on 2017-12-26.
+ */
+public class PracaDAO {
+
+    /**
+     * Metoda uzyskania informacji o Pracy z bazy danych
+     * @return Lista obiektów Zamowienia
+     * @throws SQLException Zgłasza, gdy występuje problem z zapytaniem SQL
+     * @throws ClassNotFoundException Zgłasza, gdy występuje problem z użyciem innej klasy
+     */
+    public static ObservableList<Zamowienie> pokazPraca () throws SQLException, ClassNotFoundException {
+
+        Integer war =Integer.valueOf(ConntectToDB.getCurrentUser()[0]);
+        String selectStmt = "SELECT DISTINCT z.zamowienia_id, k.imie,k.nazwisko,k.ulica, k.miejscowosc, k.nr_dom, k" +
+                ".nr_telefon, u.nazwa, u.technologia, u.predkosc, z.status, z.pracownik_id"
+                + " from zamowienia z,klienci k,pracownicy p, pakiety u " +
+                "WHERE k.klient_id = z.klient_id AND u" +
+                ".pakiet_id = z.pakiet_id and p.pracownik_id = z.pracownik_id AND z.pracownik_id =\""+ war + "\" " +
+                " AND (status LIKE 'Oczekuje' OR status LIKE 'W trakcie') GROUP BY z.zamowienia_id";
+
+        try {
+            Connection conn = ConntectToDB.Connector();
+            PreparedStatement preparedStatement = conn.prepareStatement(selectStmt);
+            ResultSet rs = preparedStatement.executeQuery(selectStmt);
+
+            ObservableList<Zamowienie> ZamowienList = getZamowienList(rs);
+
+            return ZamowienList;
+        } catch (SQLException e) {
+            System.out.println("Operacja SELECT nie powiodła się: " + e);
+
+            throw e;
+        }
+    }
+
+    /**
+     * Metoda zwraca liste obiektów ResultSet
+     * @param rs z bazy danych
+     * @return Lista obiektów Zamowienie
+     * @throws SQLException Zgłasza, gdy występuje problem z zapytaniem SQL
+     * @throws ClassNotFoundException Zgłasza, gdy występuje problem z użyciem innej klasy
+     */
+    private static ObservableList<Zamowienie> getZamowienList(ResultSet rs) throws SQLException,
+            ClassNotFoundException {
+
+        ObservableList<Zamowienie> List = FXCollections.observableArrayList();
+
+        while (rs.next()) {
+            Zamowienie z = new Zamowienie();
+            z.setZamowienia_id(rs.getInt("zamowienia_id"));
+            z.setPracownik_id(rs.getInt("zamowienia.pracownik_id"));
+            z.setImieKZ(rs.getString("klienci.imie"));
+            z.setNazwiskoKZ(rs.getString("klienci.nazwisko"));
+            z.setUlicaKZ(rs.getString("klienci.ulica"));
+            z.setNr_domKZ(rs.getString("klienci.nr_dom"));
+            z.setMiejscowoscKZ(rs.getString("klienci.miejscowosc"));
+            z.setNr_telefonKZ(rs.getString("klienci.nr_telefon"));
+            z.setNazwaPakietZ(rs.getString("pakiety.nazwa"));
+            z.setPredkoscPakietZ(rs.getString("pakiety.predkosc"));
+            z.setTechnologiaPakietZ(rs.getString("pakiety.technologia"));
+            z.setStatus(rs.getString("status"));
+
+            List.add(z);
+        }
+
+        return List;
+    }
+
+
+    /**
+     * Metoda uzyskiwania danych o konkretnym zamowieniu by uzyska informacje o statusie wykonanej pracy
+     * @param zamowienia_id - Id zamowienia do pobrania danych
+     * @return dane o pracy na podstawie zamowienia
+     * @throws SQLException Zgłasza, gdy występuje problem z zapytaniem SQL
+     */
+    public static ResultSet wyswietlDaneZamowienia( int zamowienia_id) throws SQLException {
+
+        String query = "SELECT status from zamowienia where zamowienia_id =?";
+
+        Connection conn = ConntectToDB.Connector();
+        PreparedStatement prepStmt = conn.prepareStatement(query);
+        prepStmt.setInt(1, zamowienia_id);
+        prepStmt.executeQuery();
+
+        return prepStmt.executeQuery();
+
+    }
+
+    /**
+     * Metoda aktualizacji informacji o zamowieniu w bazie danych
+     * @param zamowienia_id - Id zamowienia
+     * @param status - Status wykonania uslugi instalacji pakietu przez Pracownika-Wykonawce
+     * @throws SQLException Zgłasza, gdy występuje problem z zapytaniem SQL
+     * @throws ClassNotFoundException Zgłasza, gdy występuje problem z użyciem innej klasy
+     */
+    public static void updatePZamowienie(int zamowienia_id,String status) throws
+            SQLException, ClassNotFoundException {
+
+        String sql="UPDATE zamowienia set status=?" +
+                " WHERE zamowienia_id=?";
+
+        try {
+            Connection conn = ConntectToDB.Connector();
+            PreparedStatement prepStmt = null;
+            prepStmt = conn.prepareStatement(sql);
+            prepStmt.setString(1,status);
+            prepStmt.setInt(2,zamowienia_id);
+            prepStmt.executeUpdate();
+            prepStmt.close();
+            conn.close();
+
+        } catch (SQLException e) {
+            System.out.println("Blad przy aktualizacji danych klienta" +e);
+            throw e;
+        }
+
+    }
+}
---
+++ src/Projekt/KontroleryPracy/PracaKontroler.java
@@ -0,0 +1,202 @@
+package Projekt.KontroleryPracy;
+
+import Projekt.Modele.Zamowienie;
+import Projekt.DAO.PracaDAO;
+import Projekt.PodlaczonieDoBazy.ConntectToDB;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+
+/**
+ * Klasa kontrolera FXML zawierająca metody zarządzania zamowieniami
+ *
+ * Created by Tomek on 2017-12-26.
+ */
+public class PracaKontroler implements Initializable {
+
+    private Connection conn = null;
+    private PreparedStatement stmt = null;
+
+    @FXML
+    private TableColumn<Zamowienie, Integer> zamowienie_id_col;
+    @FXML
+    private TableColumn<Zamowienie, String> imieKZ_col;
+    @FXML
+    private TableColumn<Zamowienie, String> nazwiskoKZ_col;
+    @FXML
+    private TableColumn<Zamowienie, String> miejscowoscKZ_col;
+    @FXML
+    private TableColumn<Zamowienie, String> ulicaKZ_col;
+    @FXML
+    private TableColumn<Zamowienie, String> nrdomKz_id_col;
+    @FXML
+    private TableColumn<Zamowienie, String> nrtelKz_id_col;
+    @FXML
+    private TableColumn<Zamowienie, String> nazwaPakietZ_col;
+    @FXML
+    private TableColumn<Zamowienie, String> technologiaPK_col;
+    @FXML
+    private TableColumn<Zamowienie, String> predkoscPK_col;
+    @FXML
+    private TableColumn<Zamowienie, String> status_col;
+    @FXML
+    private TableView<Zamowienie> pracaTable = new TableView<>();
+    @FXML
+    private static int wybierzZamowienieId;
+    @FXML
+    private Button zmien;
+    @FXML
+    private Label idwybranego;
+    @FXML
+    private Label informacja;
+
+    /**
+     * Metoda wybierająca zaznaczony wiersz z tabeli
+     * @return Id zamowienia zaznaczonego w tablei pracownicy
+     */
+    public static int getSelectedPracaZamowienieId() {
+        return wybierzZamowienieId;
+    }
+
+    /**
+     * Metoda uzyskująca dane o zamowieniach jako pracy i pokazująca je w tabeli
+     * @throws SQLException Zgłasza, gdy występuje problem z zapytaniem SQL
+     * @throws ClassNotFoundException Zgłasza, gdy występuje problem z użyciem innej klasy
+     */
+    @FXML
+    private void pokazPraca() throws SQLException, ClassNotFoundException {
+
+        try {
+
+            if( ConntectToDB.getCurrentUser()[3].equals("Wykonawca")) {
+                ObservableList<Zamowienie> ZamowienieData = PracaDAO.pokazPraca();
+                miejsceWTabeliZamowienia(ZamowienieData);
+            }
+            else
+            {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Informacja");
+                alert.setHeaderText("Jesteś zalogowany jako: " + ConntectToDB.getCurrentUser()[1] + " " + ConntectToDB.getCurrentUser()[2]);
+                alert.setContentText("Nie wykonujesz żadnych usług pracująć jako " + ConntectToDB.getCurrentUser()[3]);
+
+                alert.showAndWait();
+            }
+        } catch (SQLException e) {
+            System.out.println("Wystąpił błąd podczas pobierania informacji z bazy danych.\n" + e);
+            throw e;
+        }
+    }
+
+    /**
+     * Metoda wyświetlająca pozycje w tabeli
+     * @param rptData Lista obiektów pokazywanych w tabeli
+     */
+    @FXML
+    private void miejsceWTabeliZamowienia(ObservableList<Zamowienie> rptData) {
+
+        pracaTable.setItems(rptData);
+    }
+
+    /**
+     *
+     * Metoda obsługująca zdarzenia myszy, pozwalająca użyskać id zamowienia po kliknieciu w dany wiersz tabeli
+     * Zamowienia
+     */
+    private void setValueLabel() {
+        pracaTable.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            /**
+             * Pobranie wartosci id zamowienia po kliknieciu w dany wiersz tabeli
+             * @param event nieużywany
+             */
+            @Override
+            public void handle(MouseEvent event) {
+                Zamowienie z = pracaTable.getSelectionModel().getSelectedItem();
+                idwybranego.setText(String.valueOf(z.getZamowienia_id()));
+            }
+        });
+    }
+
+    /**
+     * Metoda ustawiająca wartości w nagłówkach tabeli.
+     * @param location - nieużywany
+     * @param resources - nieużywany
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        conn = ConntectToDB.Connector();
+        zamowienie_id_col.setCellValueFactory(cellData -> cellData.getValue().zamowienia_idProperty().asObject());
+        nazwaPakietZ_col.setCellValueFactory(cellData -> cellData.getValue().nazwaPakietZProperty());
+        status_col.setCellValueFactory(cellData -> cellData.getValue().statusProperty());
+        predkoscPK_col.setCellValueFactory(cellData -> cellData.getValue().predkoscPakietZProperty());
+        technologiaPK_col.setCellValueFactory(cellData -> cellData.getValue().technologiaPakietZProperty());
+        imieKZ_col.setCellValueFactory(cellData -> cellData.getValue().imieKZProperty());
+        nazwiskoKZ_col.setCellValueFactory(cellData -> cellData.getValue().nazwiskoKZProperty());
+        ulicaKZ_col.setCellValueFactory(cellData -> cellData.getValue().ulicaKZProperty());
+        nrdomKz_id_col.setCellValueFactory(cellData -> cellData.getValue().nr_domKZProperty());
+        miejscowoscKZ_col.setCellValueFactory(cellData -> cellData.getValue().miejscowoscKZProperty());
+        nrtelKz_id_col.setCellValueFactory(cellData -> cellData.getValue().nr_telefonKZProperty());
+        pracaTable.getSelectionModel().getSelectedItems().addListener(new ListChangeListener<Zamowienie>() {
+            /**
+             * Metoda dodająca ID do każdego wiersza i wybierająca go
+             * @param c - parametr nasłuchiwający
+             */
+            public void onChanged(ListChangeListener.Change<? extends Zamowienie> c) {
+                for (Zamowienie o : c.getList()) {
+                    wybierzZamowienieId = o.getZamowienia_id();
+
+                }
+            }
+
+        });
+
+        setValueLabel();
+        czyPracownik();
+    }
+
+    /**
+     *  Metoda sprawdzajaca czy dany uzytkownik jest Wykonawca czy nie
+     */
+    @FXML
+    public void czyPracownik(){
+        if( ConntectToDB.getCurrentUser()[3].equals("Wykonawca")){
+            informacja.setText((ConntectToDB.getCurrentUser()[1]+" "+ConntectToDB.getCurrentUser
+                    ()[2]) + " ma przypisane następujące usługi: ");
+        }else {
+            informacja.setText((ConntectToDB.getCurrentUser()[1]+" "+ConntectToDB.getCurrentUser()[2])+ " nie jest pracownikiem/wykonwacą!");
+        }
+    }
+
+    /**
+     * Metoda wyświetlająca okno z możliwością modyfikacji statusu zamowienia
+     * @throws Exception Zgłasza, gdy wystapi nieoczekiwany błąd.
+     */
+    @FXML
+    public void openEdycjaModyfikacjaZamowienia() throws Exception {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../KontroleryPracy/OkienkaFNS - modyfikuj_praca.fxml"));
+
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.setTitle("Modyfikacja statusu");
+            stage.showAndWait();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
